fed56d7ef2879577d340b57857018493dccc16f3
mattramey00-dotcom <mattramey00@gmail.com>
2026-03-08 18:48:29 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 const C='tao-mc-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['/tao-monte-carlo/','/tao-monte-carlo/index.html'])));self.skipWaiting()});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./', './index.html'])));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const c=r.clone();caches.open(C).then(cache=>cache.put(e.request,c))}return r}).catch(()=>caches.match(e.request)))});
